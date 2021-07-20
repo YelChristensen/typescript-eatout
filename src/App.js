1e3a9bfaca22553fbadf7c2d639bd3e0f66f3abd
YelChristensen <yelena.christensen@gmail.com>
2021-07-20 14:45:06 +0100

    added a search field and background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,97 @@
 import React from "react";
 import Header from "./Header";
-import Fetch from "./Fetch";
+import {
+  Container,
+  IconButton,
+  InputBase,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import { alpha, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import hero from "./hero.jpg";
+
+const useStyles = makeStyles((theme) => ({
+  paperContainer: {
+    height: 750,
+    backgroundImage: `url(${hero})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundBlendMode: "overlay",
+    backgroundColor: "#ffffffbd",
+  },
+  searchPlace: {
+    padding: 60,
+    textAlign: "center",
+  },
+  search: {
+    margin: 40,
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.75),
+    "&:hover": {
+      backgroundColor: "white",
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div>
       <Header />
-      <Fetch />
+      <Paper className={classes.paperContainer}>
+        <Container maxWidth="sm" className={classes.searchPlace}>
+          <Typography variant="h5">
+            Enter your postcode to view cafes, bars and restaurants nearby
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+        </Container>
+      </Paper>
     </div>
   );
 }
