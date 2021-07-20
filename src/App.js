9065353441b5ad3c3fb769e0d8d598aee1f99398
YelChristensen <yelena.christensen@gmail.com>
2021-07-20 12:36:27 +0100

    created AppBar

--- src/App.js
+++ src/App.js
@@ -1,56 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Axios from "axios";
-import XMLParser from "react-xml-parser";
+import React from "react";
+import Header from "./Header";
+import Fetch from "./Fetch";
 
 function App() {
-  const [grubList, setGrubList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    Axios.get(
-      "https://cors-anywhere.herokuapp.com/https://ratings.food.gov.uk/search/cafe/norwich/2/30/xml",
-      {
-        headers: new Headers({
-          Accept: "text/html/xml",
-          "content-type": "application/x-www-form-urlencoded",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT",
-          "Access-Control-Allow-Headers": "Content-Type",
-        }),
-        mode: "no-cors",
-      }
-    )
-      .then((d) => {
-        let g = [];
-        const xml = new XMLParser().parseFromString(d.data);
-        let grubs = xml.children[1].children;
-        for (let i in grubs) {
-          if (
-            grubs[i].children !== null &&
-            grubs[i].children !== [] &&
-            grubs[i].children.length > 0
-          ) {
-            g.push(grubs[i].children);
-          }
-        }
-        console.log(g[0][2].value);
-        setGrubList(g);
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        setIsLoading(false);
-        console.log(e);
-      });
-  }, []);
-
-  return isLoading ? (
-    <div>Loading..</div>
-  ) : (
-    <div className="App">
-      {grubList.map((grub) => (
-        <div key={grub[2].value}>{grub[2].value}</div>
-      ))}
+  return (
+    <div>
+      <Header />
+      <Fetch />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { AppBar, Toolbar } from "@material-ui/core";
+import logo from "./LogoWhiteAndBlue.png";
 
 function Header() {
-  return <header>hero image goes here</header>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <img
+          width="200rem"
+          src={logo}
+          className="App-logo"
+          alt="Eat Out logo with Avocado"
+        />
+      </Toolbar>
+    </AppBar>
+  );
 }
 export default Header;
